8332c528854e83a940f5fd92f59d678e6b58d840
Romeo-mz <romeo.martinez@orange.fr>
2024-01-12 17:24:42 +0100

    Add main.js script to ping-pong.html

---
+++ Projet-Ping-Pong/GarnierTP1/GarnierTP1/code/js/projet/main.js
@@ -0,0 +1,6 @@
+// main.js
+
+import { init } from 'ping-pong.js';
+
+// Call the init function after the document has loaded
+document.body.onload = init;
--- Projet-Ping-Pong/GarnierTP1/GarnierTP1/code/js/projet/ping-pong.js
+++ Projet-Ping-Pong/GarnierTP1/GarnierTP1/code/js/projet/ping-pong.js
@@ -1,13 +1,12 @@
 // Import functions
 
-import { createTable } from "./table.js";
+import { createTable } from './table.js';
 
-// import { createTable } from './table.js';
 // import { createBall } from './ball.js';
 
 // Set up the scene
 
-function init() {
+export function init() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
     const renderer = new THREE.WebGLRenderer();
